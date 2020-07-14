dcf7c3e3e94983f357e17039de6b848753a1891a
Andrew Cai <andrew@yugabyte.com>
2020-07-14 11:47:08 -0700

    Add entire url to asset prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ if(process.env.GATSBY_STAGE && process.env.GATSBY_STAGE==='production') {
   )
 }
 module.exports = {
-  pathPrefix: `/2019`,
+  assetPrefix: `https://wonderful-bell-083eb2.netlify.app/2019`,
   siteMetadata: {
     title: "Distributed SQL",
     author: "YugaByte Inc",
