ddfcf37d4d6bea216b4766e0274761708b4b56f1
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2022-01-14 13:24:48 +0500

    Cleanup gatsby-node.js by removing comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,3 @@
-// const path = require('path');
-// const conferenceData = require('./src/data/conferenceData');
-// const speakerData = require('./src/data/data.json');
-
 exports.onCreateWebpackConfig = async ({ actions }) => {
   var webpackCustomConfig = null
   if (process.env.GATSBY_STAGE === 'production') {
@@ -13,24 +9,3 @@ exports.onCreateWebpackConfig = async ({ actions }) => {
     ...webpackCustomConfig,
   })
 }
-
-/*
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage, createNode } = actions;
-  // const speakers = JSON.parse( JSON.stringify(speakerData) );
-  const sessionPageTemplate = path.resolve('src/page-templates/session.js');
-  // conferenceData.forEach(confDaySessions => confDaySessions.forEach(talk => {
-  //   try {
-  //     createPage({
-  //       path: `/session/${talk.id}`,
-  //       component: sessionPageTemplate,
-  //       context: {
-  //         ...talk
-  //       },
-  //     })
-  //   } catch (error) {
-
-  //   }
-  // }));
-}
-*/
